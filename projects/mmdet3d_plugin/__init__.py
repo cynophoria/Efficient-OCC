9bceec03fc42074b07476b8cb6b0a8888e2f22d7
shaoqing.wu <shaoqing_wu@foxmail.com>
2024-03-26 13:57:12 +0800

    fix benchmark bug

--- projects/mmdet3d_plugin/__init__.py
+++ projects/mmdet3d_plugin/__init__.py
@@ -5,6 +5,7 @@ from .core.evaluation.eval_hooks import CustomDistEvalHook
 from .datasets.pipelines import (
     PhotoMetricDistortionMultiViewImage, PadMultiViewImage,
     NormalizeMultiviewImage, CustomCollect3D)
+from .efficientocc import *
 from .models.backbones.vovnet import VoVNet
 from .models.hooks import *
 from .models.opt.adamw import AdamW2
